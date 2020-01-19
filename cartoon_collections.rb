b0e4cd42a8539d453e8ed3bebc3930b5694354b8
Phoenix R <phoenixrabang@gmail.com>
2020-01-19 23:47:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,14 +6,22 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(array)
-  # Your code here
+  x = []
+  array.map do |element|
+    x << "#{element.capitalize}!"
+  end
+  x
 end
 
 def long_planeteer_calls(array)
-  # Your code here
+  array.any? do |word|
+    if word.length > 4
+      return true
+    end
+  end
 end
 
 def find_the_cheese(array)
-  # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find 
 end
