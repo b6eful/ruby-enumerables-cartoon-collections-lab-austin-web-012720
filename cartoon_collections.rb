5b39555d4647e7c63cd9a0b4cd2845803ef6d9c4
Phoenix R <phoenixrabang@gmail.com>
2020-01-19 23:42:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,15 +1,17 @@
-def roll_call_dwarves(dwarf)
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |dwarf, index|
+    k = k +1
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)
   # Your code here
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)
   # Your code here
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
